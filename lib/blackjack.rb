90b46878ff8efe285fc13ed97203534a3f23811d
Kelsey Low <kklowlow@gmail.com>
2018-12-25 04:22:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,7 +30,7 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
+  user_input = get_user_input
   if get_user_input == 'h'
     card_total += deal_card
   elsif get_user_input != 'h' && get_user_input != 's'
