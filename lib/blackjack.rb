02bc19b1e93c3f9173c977df135d9563da53ba62
Kelsey Low <kklowlow@gmail.com>
2018-12-25 04:13:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,10 @@ def hit?(card_total)
 end
 
 def invalid_command
-  # code invalid_command here
+  if get_user_input != 'h' && get_user_input != 's'
+    puts "Please enter a valid command"
+  end
+  prompt.user
 end
 
 #####################################################
